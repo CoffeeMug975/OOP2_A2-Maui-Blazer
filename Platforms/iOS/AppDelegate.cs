d575312818a2d6d264aadc343a2fe4f4c946c977
DSAsefa <danielasefa975@gmail.com>
2024-07-13 22:41:22 -0600

    push
    push

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OOP2_A2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
