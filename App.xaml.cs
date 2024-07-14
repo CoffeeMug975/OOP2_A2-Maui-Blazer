28885f9484b0393b43f5bfd98a0e5c5e261489fc
Daniel A <156850549+CoffeeMug975@users.noreply.github.com>
2024-07-13 22:36:31 -0600

    Add files via upload
    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP2_A2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP2_A2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
